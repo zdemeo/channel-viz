9e6834eee250e29096bcfa3ad252877a841a7a3f
zdemeo <zdemeo@bu.edu>
2014-08-20 20:12:06 -0700

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,9 +12,9 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
+	var defaultKey		= 'Ku7NdCNaFealXLL7LPlWHDOTPBUpz8eQiZnIcPJFhZZTo8Xn', // Unique master Xively API key to be used as a default
+		defaultFeeds	= ['1565805802'], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'Test', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
