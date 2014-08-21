361f0bee7696d6c9e21b9eb0c23a1f19138c9052
zdemeo <zdemeo@bu.edu>
2014-08-20 20:15:26 -0700

    Update follows.js

--- follows.js
+++ follows.js
@@ -14,11 +14,11 @@
 
 	var defaultKey		= 'Ku7NdCNaFealXLL7LPlWHDOTPBUpz8eQiZnIcPJFhZZTo8Xn', // Unique master Xively API key to be used as a default
 		defaultFeeds	= ['1565805802'], // Comma separated array of Xively Feed ID numbers
-		applicationName	= 'Test', // Replaces Xively logo in the header
+		applicationName	= 'Weatherstation', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
-		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
-		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
+		dataColor		= '0D00FF', // CSS HEX value of color to represent data (omit leading #)
+		hideForm		= 1; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
 
